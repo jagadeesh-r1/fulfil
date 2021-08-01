3498c64c09663ae1399af80cbe8e3e94b3e1a40c
jagadeesh-r1 <mail4jagadeeshreddy@gmail.com>
2021-08-01 15:59:50 +0530

    changed Debug to False and increased default timeout for sqlite3

--- fulfil/settings.py
+++ fulfil/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4y52fj@77=y!sr2g3llwsc(5sbd6oct3$7l2a-gqcu621*jy+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0"]
 
 
 # Application definition
@@ -84,6 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': str(BASE_DIR / 'db.sqlite3'),
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'OPTIONS': {
+        'timeout': 40,
     }
 }
 
