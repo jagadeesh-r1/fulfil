645bb02f45a38ad5571f5bcebbb4aa1780646567
jagadeesh-r1 <mail4jagadeeshreddy@gmail.com>
2021-08-01 19:39:52 +0530

    updated hosts

--- fulfil/settings.py
+++ fulfil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '47dxk$d)@wh#44-=ixm_6jrktts@$3a9)3qcr(l3dg^zc&=hk0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["20.198.112.178","0.0.0.0","127.0.0.1"]
 
 
 # Application definition
